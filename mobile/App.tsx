e9445f80d72e16ca28f34dbc6e6e53ebdba7560f
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-18 12:07:47 -0300

    mobile hero section

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import {
   useFonts,
@@ -11,6 +11,7 @@ import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
@@ -29,10 +30,33 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900"
+      className="relative flex-1 items-center bg-gray-900 px-8"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
