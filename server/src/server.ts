be5bcdd7212cc0a59d77e6c2d08ba7eae0034f15
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-18 19:48:24 -0300

    server CRUD of memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,14 @@
-import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import fastify from "fastify";
+import { memoriesRoutes } from "./routes/memories";
 
-const app = fastify()
-const prisma = new PrismaClient()
+const app = fastify();
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+    console.log("🚀 HTTP server running on http://localhost:3333");
+  });
