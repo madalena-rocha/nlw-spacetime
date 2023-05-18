43f8e7f1da4cd131b841183694fbf9bb24be7d96
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-18 10:04:58 -0300

    web copyright

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -50,6 +50,19 @@ export default function Home() {
             CADASTRAR LEMBRANÇA
           </a>
         </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       {/* Right */}
