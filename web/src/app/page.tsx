bf8392d0935bb26c168b65bd8a7058bcc3c3c187
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-19 10:44:24 -0300

    github OAuth and github code

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
         {/* Sign In */}
         <a
-          href=""
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
         >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
