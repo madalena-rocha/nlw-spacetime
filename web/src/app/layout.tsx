cff68f05a404cd5f4620b7bcefb55c4411e1f06e
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-20 19:43:41 -0300

    web logout route

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
+
 import { Hero } from '../components/Hero'
 import { Profile } from '../components/Profile'
 import { SignIn } from '../components/SignIn'
