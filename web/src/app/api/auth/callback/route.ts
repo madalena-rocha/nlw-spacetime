848426dea9aabdd53fc22368c2dd7485dc5937bc
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-20 13:07:55 -0300

    web displaying user profile

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   })
 }
